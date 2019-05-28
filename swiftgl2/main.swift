d598674768cecaa0f65c82a42250a3829acf88f0
mfernba <athlonford@hotmail.com>
2019-05-28 17:20:17 +0200

    Initial Commit

---
+++ swiftgl2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftgl2
+//
+//  Created by Manuel Fernández Baños on 28/5/19.
+//  Copyright © 2019 Manuel Fernández Baños. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
