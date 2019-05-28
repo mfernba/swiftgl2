854ae52cbc5535afd9cfe877d927b2d4c2ced05e
mfernba <athlonford@hotmail.com>
2019-05-28 17:56:55 +0200

    Initial commit

---
+++ swiftgl2/windowut.swift
@@ -0,0 +1,9 @@
+//
+//  windowut.swift
+//  swiftgl2
+//
+//  Created by Manuel Fernández Baños on 28/5/19.
+//  Copyright © 2019 Manuel Fernández Baños. All rights reserved.
+//
+
+import Foundation
