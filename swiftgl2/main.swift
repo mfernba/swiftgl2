85f56c344f70065323c3b38a1fa73f91e65cad48
mfernba <athlonford@hotmail.com>
2019-05-28 17:56:13 +0200

    Initial implementation

--- swiftgl2/main.swift
+++ swiftgl2/main.swift
@@ -6,7 +6,42 @@
 //  Copyright © 2019 Manuel Fernández Baños. All rights reserved.
 //
 
-import Foundation
+import AppKit
+import Cocoa
 
-print("Hello, World!")
+func computeScreenCenteredPosition(_ windowWidth : CGFloat, _ windowHeight : CGFloat) -> NSRect {
+    
+    let screen : NSScreen = NSScreen.main!
+    
+    let screenFrame : NSRect = screen.frame;
+    
+    let windowWidth : CGFloat = 200.0
+    let windowHeight : CGFloat = 200.0
+    let windowX = screenFrame.origin.x + 0.5 * screenFrame.width - 0.5 * windowWidth;
+    let windowY = screenFrame.origin.y + 0.5 * screenFrame.height - 0.5 * windowHeight;
+    
+    return NSRect(x: windowX, y: windowY, width: windowWidth, height: windowHeight)
+}
+
+func makeMainWindow() -> NSWindow {
+    
+    let windowWidth : CGFloat = 200.0
+    let windowHeight : CGFloat = 200.0
+    let windowRect : NSRect = computeScreenCenteredPosition(windowWidth, windowHeight)
+    
+    let windowStyleMask = NSWindow.StyleMask(rawValue: NSWindow.StyleMask.titled.rawValue
+            | NSWindow.StyleMask.closable.rawValue
+            | NSWindow.StyleMask.resizable.rawValue)
+    
+    return NSWindow.init(contentRect: windowRect, styleMask: windowStyleMask, backing: NSWindow.BackingStoreType.buffered, defer: true)
+}
+
+let app : NSApplication = NSApplication.shared
+
+let mainWindow = makeMainWindow();
+mainWindow.makeKeyAndOrderFront(app)
+
+app.run()
+
+
 
